46377e11da8941862c44112149c9cc7a24868875
donalddellapietra <Donald.della.pietra@gmail.com>
2024-11-21 16:49:50 -0500

    Update test_manual.py

--- project/test_manual.py
+++ project/test_manual.py
@@ -5,19 +5,19 @@ import numpy as np
 from minitorch.tensor import Tensor
 
 # Define two matrices
-matrix_a = np.array([
+matrix_a = Tensor[
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9],
     [10, 11, 12],
     [13, 14, 15]
-])
+]
 
-matrix_b = np.array([
+matrix_b = Tensor[
     [1, 2, 3, 4],
     [5, 6, 7, 8],
     [9, 10, 11, 12]
-])
+]
 
 # Convert numpy arrays to minitorch Tensors
 tensor_a = Tensor(matrix_a)
